c5c25e3898bfa8e5f86644f38ea4e1581942582c
JASONIYI <jasoniyi@JASONIYIs-MacBook-Pro.local>
2022-12-10 16:36:25 +0100

    created the testimonial component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { faBellSlash } from "@fortawesome/free-regular-svg-icons";
 import Banner from "./components/Banner";
 import About from "./components/About";
 import Category from "./components/Category";
+import Testimonials from "./components/testimonials";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Banner />
       <About />
       <Category />
+      <Testimonials />
     </div>
   );
 }
